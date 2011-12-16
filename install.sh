04827a3957a73a79ff504e887e79c55569884b88
Stan Hutcheon <stanley.h@hotmail.co.uk>
2011-12-16 19:47:56 +0000

    updated readme and fixed permissions

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 echo "This installs everything you need to get this Siri auth server working."
 echo "I will now begin..."
+sudo chmod -R 770 ./
 sudo ./scripts/dependencies.sh
 sudo ./scripts/certs.sh
 echo "Everything is done. After you add iPhone 4S auth keys into siriProxy.rb, just type sudo ruby ./start.rb in terminal :)"
