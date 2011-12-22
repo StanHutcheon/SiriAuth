37675b459e754d98ef6925340aebe0344e93c9b5
Stanley Hutcheon <stanley.h@hotmail.co.uk>
2011-12-22 20:27:39 +0000

    install script didnt work, individual scripts added.

--- dependencies.sh
+++ dependencies.sh
@@ -1,7 +1,7 @@
 echo "refreshing apt (just in case)..."
 sudo apt-get update
 echo "installing required stuff etc..."
-sudo apt-get install ruby build-essential openssl libreadline6 libreadline6-dev curl git-core zlib1g zlib1g-dev libssl-dev libyaml-dev libsqlite3-0 libsqlite3-dev sqlite3 libxml2-dev libxslt-dev autoconf libc6-dev ncurses-dev automake libtool bison subversion
+sudo apt-get install nano ruby build-essential openssl libreadline6 libreadline6-dev curl git-core zlib1g zlib1g-dev libssl-dev libyaml-dev libsqlite3-0 libsqlite3-dev sqlite3 libxml2-dev libxslt-dev autoconf libc6-dev ncurses-dev automake libtool bison subversion
 echo "Installing RVM..."
 bash < <(curl -s https://raw.github.com/wayneeseguin/rvm/master/binscripts/rvm-installer)
 [[ -s "$HOME/.rvm/scripts/rvm" ]] && . "$HOME/.rvm/scripts/rvm"
--- install.sh
+++
@@ -1,8 +0,0 @@
-echo "This installs everything you need to get this Siri auth server working."
-echo "I will now begin..."
-sudo chmod -R 770 ./
-cd scripts
-sudo ./dependencies.sh
-sudo ./certs.sh
-cd ../
-echo "Everything is done. After you add iPhone 4S auth keys into siriProxy.rb, just type sudo ruby ./start.rb in terminal :)"
